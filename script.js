01f2dc73e0efe68aa923c528a0401cef67fce67a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-20 21:35:04 +0200

    Make work with video

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /* global Peer */
 
-console.log('getting code');
-
 /**
  * VARIABLES
  */
@@ -10,20 +8,26 @@ console.log('getting code');
  * Instantiate a new Peer, passing to it an alphanumeric string as an ID and options obj
  * @type {Peer}
  */
-const peer = new Peer(''+Math.floor(Math.random()*2**18).toString(36).padStart(4,0), {
+const peer = new Peer(
+  '' +
+    Math.floor(Math.random() * 2 ** 18)
+      .toString(36)
+      .padStart(4, 0),
+  {
     host: location.hostname,
     debug: 1,
-    path: '/myapp'
-});
+    path: '/myapp',
+  }
+)
 
-window.peer = peer;
+window.peer = peer
 
-const callBtn = document.querySelector('.call-btn');
-const audioContainer = document.querySelector('.call-container');
-const hangUpBtn = document.querySelector('.hangup-btn');
+const callBtn = document.querySelector('.call-btn')
+const callContainer = document.querySelector('.call-container')
+const hangUpBtn = document.querySelector('.hangup-btn')
 
-let conn;
-let code;
+let conn
+let code
 
 /**
  * FUNCTIONS
@@ -34,8 +38,8 @@ let code;
  * @returns {string} - the code retrieved
  */
 function getStreamCode() {
-    code = window.prompt('Please enter the sharing code');
-    return code;
+  code = window.prompt('Please enter the sharing code')
+  return code
 }
 
 /**
@@ -45,14 +49,20 @@ function getStreamCode() {
  */
 
 function getLocalStream() {
-    const constraints = {video: false, audio: true}
+  const constraints = {
+    video: true,
+    audio: true,
+  }
 
-    navigator.mediaDevices.getUserMedia(constraints).then( stream => {
-        console.log(stream);
-        setLocalStream(stream);
-    }).catch( err => {
-        console.log("u got an error:" + err)
-    });
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      console.log(stream)
+      setLocalStream(stream)
+    })
+    .catch(err => {
+      console.error('u got an error:' + err)
+    })
 }
 
 /**
@@ -62,9 +72,9 @@ function getLocalStream() {
  */
 
 function setLocalStream(stream) {
-    window.localAudio.srcObject = stream;
-    window.localAudio.autoplay = true;
-    window.localStream = stream;
+  window.localVideo.srcObject = stream
+  window.localVideo.autoplay = true
+  window.localStream = stream
 }
 
 /**
@@ -73,9 +83,9 @@ function setLocalStream(stream) {
  * @returns {void}
  */
 function setRemoteStream(stream) {
-    window.remoteAudio.srcObject = stream;
-    window.remoteAudio.autoplay = true;
-    window.peerStream = stream;
+  window.remoteVideo.srcObject = stream
+  window.remoteVideo.autoplay = true
+  window.peerStream = stream
 }
 
 /**
@@ -83,9 +93,9 @@ function setRemoteStream(stream) {
  * @returns{void}
  */
 function showConnectedContent() {
-    window.caststatus.textContent = `You're connected`;
-    callBtn.hidden = true;
-    audioContainer.hidden = false;
+  window.caststatus.textContent = `You're connected`
+  callBtn.hidden = true
+  callContainer.hidden = false
 }
 
 /**
@@ -93,9 +103,9 @@ function showConnectedContent() {
  * @returns{void}
  */
 function showCallContent() {
-    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
-    callBtn.hidden = false;
-    audioContainer.hidden = true;
+  window.caststatus.textContent = `Your device ID is: ${peer.id}`
+  callBtn.hidden = false
+  callContainer.hidden = true
 }
 
 /**
@@ -104,7 +114,7 @@ function showCallContent() {
  */
 
 function connectPeers() {
-    conn = peer.connect(code)
+  conn = peer.connect(code)
 }
 
 /**
@@ -114,68 +124,65 @@ function connectPeers() {
 /**
  * Get the connection code, connect peers and create a call
  */
-callBtn.addEventListener('click', function(){
-    getStreamCode();
-    connectPeers();
-    const call = peer.call(code, window.localStream);
-    /**
-     * when the call is streaming, set the remote stream for the caller
-     */
-    call.on('stream', function(stream) {
-        showConnectedContent();
-        console.log(peer);
-        setRemoteStream(stream);
-    });
+callBtn.addEventListener('click', function () {
+  getStreamCode()
+  connectPeers()
+  const call = peer.call(code, window.localStream)
+  /**
+   * when the call is streaming, set the remote stream for the caller
+   */
+  call.on('stream', function (stream) {
+    showConnectedContent()
+    console.log(peer)
+    setRemoteStream(stream)
+  })
 })
 
 /**
  * Close the connection between peers
  */
-hangUpBtn.addEventListener('click', function (){
-    conn.close();
-    showCallContent();
+hangUpBtn.addEventListener('click', function () {
+  conn.close()
+  showCallContent()
 })
 
 /**
  * When the peer has connected to the server, diplay the peer ID
  */
 peer.on('open', function () {
-    console.log('ready to receive cast')
-    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
-});
+  console.log('ready to receive cast')
+  window.caststatus.textContent = `Your device ID is: ${peer.id}`
+})
 
 /**
  * When a data connection between peers is open, get the connecting peer's details
  */
-peer.on('connection', function(connection){
-    conn = connection;
-    peer_id = connection.peer;
-});
+peer.on('connection', function (connection) {
+  conn = connection
+  peer_id = connection.peer
+})
 
 /**
  * When a call has been created, answer it and set the remote stream for the person being called
  */
-peer.on('call', function(call) {
+peer.on('call', function (call) {
+  const answerCall = confirm('Do you want to answer?')
 
-    const answerCall = confirm("Do you want to answer?")
+  if (answerCall) {
+    call.answer(window.localStream)
+    showConnectedContent()
+    call.on('stream', function (stream) {
+      console.log('Stream Received')
+      setRemoteStream(stream)
+    })
+    conn.on('close', function () {
+      showCallContent()
+    })
+  } else {
+    console.log('call denied')
+  }
+})
 
-    if(answerCall){
-        call.answer(window.localStream)
-        showConnectedContent();
-        call.on('stream', function(stream) {
-            console.log('Stream Received');
-            setRemoteStream(stream);
-        });
-        conn.on('close', function (){
-            showCallContent();
-        })
-    } else {
-        console.log("call denied");
-    }
-});
+peer.on('error', err => console.error(err))
 
-
-
-peer.on('error', err => console.error(err));
-
-getLocalStream();
+getLocalStream()
